5db61088d0eec4a7729682f5ea093f0f97270919
olegtempl <linner_952@mail.ru>
2018-06-10 23:46:39 +0200

    update tests

--- tests/jasmine/spec.js
+++ tests/jasmine/spec.js
@@ -1,49 +1,31 @@
 // for basic functions tests
 const   path = '../../build/js/',		// путь до файла
-		testFile = require(`${path}index`);	// импортируем модуль
-// for cli tests
-const fileCli = '';
-const apiCli = {
-  v : '-v'
-};
+    testFile = require(`${path}index`);	// импортируем модуль
+// const path = ;
 /*
   Basic functions tests
 */
-describe("Jasmine", function() {
-  it("makes testing JavaScript awesome!", function() {
-    const count = testFile(1);				// экземпляр модуля для этого теста
-    expect(count).toBe(2); 				    // сам тест
-  });
-});
-/*
-  Command-line interface tests ( add rights the performing file - ex: chmod 755 index.js)
-*/
-const { spawn } = require('child_process');
-function runCmd(cmd, args, callBack) {
-  const child = spawn(cmd, args);
+// describe("Jasmine", function() {
+//   it("", function() {
 
-  let resp = "";
-  child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-  child.stdout.on('end', function() { callBack (resp) });
-  child.stdout.on('error', callBack);
-};
-describe("Command-line interface tests", function() {
-  it("Call the file with flag ''", function() {
-    runCmd(fileCli , [apiCli.v], function(err, text) {
-      if (err) return console.error(err);
-      console.log(text)
-     });
+//   });
+// });
+
+describe("Files in directory ", function () {
+  it("generation the sh files", function () {
+    setTimeout(function () {
+      genBundle()
+    }, 1000);
+    fs.watchFile(path, function (curr, prev) {
+      expect(curr.nlink != prev.nlink).toBe(true)
+    });
   });
-  it("Call the file with flag ''", function() {
-    runCmd(fileCli , [apiCli.], function(err, text) {
-      if (err) return console.error(err);
-      console.log(text)
-     });
-  });
-  it("Call the file with flag ''", function() {
-    runCmd(fileCli , [apiCli.], function(err, text) {
-      if (err) return console.error(err);
-      console.log(text)
-     });
-  });
+  // it("Synchronization modules in docs folder", function () {
+  //   setTimeout(function () {
+  //     gen.docs()
+  //   }, 1000);
+  //   fs.watch(path, (eventType, filename) => {
+  //     expect(eventType).toBe(true)
+  //   });
+  // });
 });
